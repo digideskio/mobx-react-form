82f2838e7eca938dd9b7068fc6ba558f0809f2d4
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-16 04:06:48 +0600

    App receiving state from index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import Tabs from './Tabs';
 
+@observer
 class App extends Component {
+    getChildContext() {
+        const { store } = this.props;
+
+        return {
+            formGet: path => store.form.get(path),
+            formSet: (path, value) => store.form.set(path, value)
+        };
+    }
+
     render() {
+        console.log('app render..');
+        const { store } = this.props;
+
         return (
             <div>
-                Hello world
+                <Tabs tabs={store.form.schema} />
             </div>
         );
     }
 }
 
-App.propTypes = {};
-App.defaultProps = {};
+App.childContextTypes = {
+    formGet: React.PropTypes.func,
+    formSet: React.PropTypes.func
+};
+
+App.contextTypes = {
+    store: React.PropTypes.object
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import store from './store';
 import App from './components/App';
+import Provider from './Provider';
 
 const rootEl = document.getElementById('root');
+
 ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  rootEl
+    <AppContainer>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </AppContainer>,
+    rootEl
 );
 
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./components/App').default;
-    ReactDOM.render(
-      <AppContainer>
-         <NextApp />
-      </AppContainer>,
-      rootEl
-    );
-  });
+    module.hot.accept('./components/App', () => {
+        // If you use Webpack 2 in ES modules mode, you can
+        // use <App /> here rather than require() a <NextApp />.
+        const NextApp = require('./components/App').default;
+
+        ReactDOM.render(
+            <AppContainer>
+                <Provider store={store}>
+                    <NextApp />
+                </Provider>
+            </AppContainer>,
+            rootEl
+        );
+    });
 }
