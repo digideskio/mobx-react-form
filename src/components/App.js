8bee82152a5cb6e14e1512d30d684de8e13314ac
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-15 22:41:02 +0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello world
+            </div>
+        );
+    }
+}
+
+App.propTypes = {};
+App.defaultProps = {};
+
+export default App;
