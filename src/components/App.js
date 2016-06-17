1acef179ca3007c6d53fd118bac8e731bcdcb4f1
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-17 12:13:05 +0600

    working app

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import Tabs from './Tabs';
+
+import { FormModel, FormComponent } from '../Form';
+
+const data = {
+    tab1: {
+        title: 'hello world',
+        description: 'this is a revolution',
+        slides: [
+            {title: 'slide1 title', description: 'slide1 description'},
+            {title: 'slide2 title', description: 'slide2 description'}
+        ]
+    }
+};
+
+const schema = {
+    tab1: {
+        label: 'Tab 1',
+        controls: {
+            title: { label: 'Title', type: 'text', default: '' },
+            description: { label: 'Description', type: 'textarea', default: '' },
+            slides: {
+                label: 'Slides',
+                type: 'group-repeater',
+                default: [
+                    { title: 'slide 1 title', 'description': 'slide 1 desc' }
+                ],
+                schema: {
+                    title: { label: 'Title', type: 'text', default: '' },
+                    description: { label: 'Description', type: 'textarea', default: '' }
+                }
+            }
+        }
+    }
+};
 
 @observer
 class App extends Component {
-    getChildContext() {
-        const { store } = this.props;
+    state = {
+        form: new FormModel(schema, data)
+    };
 
-        return {
-            formGet: path => store.form.getIn(path),
-            formSet: (path, value) => store.form.setIn(path, value)
-        };
-    }
+    renderData = () => {
+        return <pre>{
+            JSON.stringify(this.state.form.data, null, 2)
+        }</pre>;
+    };
 
     render() {
-        console.log('app render..');
-        const { store } = this.props;
-
         return (
             <div>
-                <Tabs tabs={store.form.schema} />
+                {this.renderData()}
+
+                <FormComponent form={this.state.form} />
             </div>
         );
     }
