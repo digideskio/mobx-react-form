d2a79e8d0b7261e61959344652f401bdcf141821
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-17 12:26:00 +0600

    updated package and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,44 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    devtool: 'eval',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file?name=[path][name].[ext]'
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader?sourceMap']
+            },
+            {
+                test: /\.less/,
+                loaders: ['less-loader','style-loader', 'css-loader?sourceMap']
+            },
+            {
+                test: /\.(png|jpeg|jpg|mp3|ogg|ico)$/,
+                loader: 'file?name=[path][name].[ext]'
+            }
+        ]
+    }
 };
