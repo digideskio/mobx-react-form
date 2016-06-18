45d1da00a77583f59be1be73b2d6ded83ad291cc
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-19 02:58:19 +0600

    export grouped components

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,2 +1,5 @@
 export FormModel from './models/Form';
 export FormComponent from './components/Form';
+
+export GroupedFormModel from './models/GroupedForm';
+export GroupedFormComponent from './components/GroupedForm';
